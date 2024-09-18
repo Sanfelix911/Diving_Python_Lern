cee66f85712210e38200389f96ca77530d7dd799
Sanfelix <sanfelix@yandex.ru>
2024-09-18 20:32:25 +0900

    Семинар 2  Задания1,2 решены

---
+++ SEMINAR/Sem2_2.py
@@ -0,0 +1,24 @@
+#Задание 1
+# a = 12
+# b = 'Hello'
+# c = 3.14
+# d = True
+
+# print(f'{type(a)=}')
+# print(f'{type(b)=}')
+# print(f'{type(c)=}')
+# print(f'{type(d)=}')
+
+
+
+# #Задание2
+# import sys
+# data = [42, 73.0, 'Hello World', True, 'Hello World', 256, 2 ** 8, 'Привет Мир']
+
+
+# for nn, value in enumerate(data,1):
+#     check_int = 'Похож на целое число' if isinstance(value, int) else ''
+#     check_str = 'Похож на строку' if isinstance(value, str) else ''
+#     print(f'{nn}. Значение {value}.\nАдрес в памяти {id(value)}.\tРазмер в памяти {sys.getsizeof(value)}).'
+#           f'\t Хэш объекта {hash(value)}.\t{check_int}{check_str}')
+
