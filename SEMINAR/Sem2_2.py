9b2b894eb24088969c4b13696c526825b0a91a5d
Sanfelix <sanfelix@yandex.ru>
2024-09-18 23:52:37 +0900

    Сем2 Задание про банкомат решено

--- SEMINAR/Sem2_2.py
+++ SEMINAR/Sem2_2.py
@@ -70,4 +70,71 @@
 #     x1 = (-b + d**0.5) / (2 * a)
 #     x2 = (-b - d**0.5) / (2 * a)
 #     rezult = f'У уравнения 2 комплексных корня, {x1=}, {x2=}'
-# print(rezult)    
+# print(rezult) 
+# 
+
+import decimal
+decimal.getcontext().prec = 2
+
+CMD_DEPOSIT = 'п'
+CMD_WISD = 'с'
+CMD_EXIT = 'в'
+RICHNESS_SUM = decimal.Decimal(5_000_000)
+RICHNESS_TAX = decimal.Decimal(10)/decimal.Decimal(100)
+WIDR_TAX = decimal.Decimal(15)/decimal.Decimal(1000)
+ADD_PERCENT = decimal.Decimal(3)/decimal.Decimal(100)
+MULTIP = 50
+MIN_REM = 30
+MAX_REM = 600
+COUNT_OPER = 3
+
+account = decimal.Decimal(0)
+count = 0
+
+while True:
+    command = input(f'Пополнить - "{CMD_DEPOSIT}", Снять - "{CMD_WISD}", Выйти - "{CMD_EXIT}" ')
+    if command == CMD_EXIT:
+        print(f'Возьмите карту на которй {account}')
+        break
+    if account > RICHNESS_SUM:
+        percent = account * RICHNESS_TAX
+        account = account - percent
+        print(f'Удержан налог на богатство {RICHNESS_TAX}% в размере {percent}\n'
+              f'Итого на карте осталось {account}')
+    if command in (CMD_DEPOSIT, CMD_WISD):
+        ammaunt = 1
+        while ammaunt % 50 != 0:
+            ammaunt = int(input(f'Введите сумму кратную {MULTIP}:'))
+    if command == CMD_DEPOSIT:
+        account += ammaunt
+        count += 1
+        print(f'Пополнение карты на {ammaunt}\n'
+              f'На карте {account}')  
+    elif command == CMD_WISD:
+        wiz_percent = ammaunt * WIDR_TAX
+        wiz_percent = (MIN_REM if wiz_percent < MIN_REM else 
+                       MAX_REM if wiz_percent > MAX_REM else wiz_percent)
+        if account >= ammaunt + wiz_percent :
+            count += 1
+            account -= (ammaunt + wiz_percent)
+            print(f'Снятие с карты на {ammaunt} процент за снятие {wiz_percent}\n'
+              f'На карте {account}')
+        else:
+            print(f'Не достаточно средсв для снатия {ammaunt}\n'
+                  f'Сумма снятия {ammaunt}? комиссия {wiz_percent}\n'
+                  f'На карте {account}')
+    if count % COUNT_OPER == 0:
+        bonus_prcent = account * ADD_PERCENT
+        account += bonus_prcent
+        print(f'На счет начисленно {ADD_PERCENT}% в сумме {bonus_prcent}\n'
+              f'На карте {account}')
+
+
+            
+            
+            
+
+
+
+    
+                
