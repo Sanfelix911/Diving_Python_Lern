1ef1aad4719768e0cb8c53a9d94dcef5cb3f3c12
Sanfelix <sanfelix@yandex.ru>
2024-09-18 21:05:20 +0900

    Сем2 Задание 3 решено

--- SEMINAR/Sem2_2.py
+++ SEMINAR/Sem2_2.py
@@ -22,3 +22,18 @@
 #     print(f'{nn}. Значение {value}.\nАдрес в памяти {id(value)}.\tРазмер в памяти {sys.getsizeof(value)}).'
 #           f'\t Хэш объекта {hash(value)}.\t{check_int}{check_str}')
 
+# #Задание3
+
+# BIN = 2
+# OCT = 8
+
+# num = int(input('Введите число - '))
+
+# for div in (BIN,OCT):
+#     result: str = ''
+#     test_num: int = num
+#     while test_num > 0:
+#         result = str(test_num % BIN) + result
+#         test_num //= div
+#     print(f'For {div=} {result=}')
+# print(f'Двоичное число {bin(num)}, Восьмеричное число {oct(num)}')        
