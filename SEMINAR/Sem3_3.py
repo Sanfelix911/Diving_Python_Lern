dd99498f856fe24291a1e1f8b9e529f9446e0989
Sanfelix <sanfelix@yandex.ru>
2024-09-19 22:01:52 +0900

    Сем3 Задание 5 решено

--- SEMINAR/Sem3_3.py
+++ SEMINAR/Sem3_3.py
@@ -45,3 +45,14 @@
 #             data.remove(item)
 # print(data)
 
+# Задание 5
+
+# data = [42, 73 , 5, 42, 42, 2, 3, 7, 73, 42]
+
+# new_data = []
+# for nn, item  in enumerate(data, 1 ):
+#     if item %2 != 0:
+#         new_data.append(nn)
+# print(new_data)
+
+#
