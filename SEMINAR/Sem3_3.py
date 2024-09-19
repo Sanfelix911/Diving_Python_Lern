fd60a54e41197bacd73fa9a2b8991457f225ea13
Sanfelix <sanfelix@yandex.ru>
2024-09-19 21:18:50 +0900

    Задание 2 решено

--- SEMINAR/Sem3_3.py
+++ SEMINAR/Sem3_3.py
@@ -3,9 +3,23 @@
 # unique_data = list(set(data))
 # print(unique_data)
 
-data = [42, 73 , 5, 42, 42, 2, 3, 7, 73, 42]
-unique_data = []
-for data in data:
-    if data not in unique_data:
-        unique_data.append(data)
-print(unique_data)
+# data = [42, 73 , 5, 42, 42, 2, 3, 7, 73, 42]
+# unique_data = []
+# for data in data:
+#     if data not in unique_data:
+#         unique_data.append(data)
+# print(unique_data)
+
+# Задание 2
+# data = input( 'Введите данные: ')
+
+# if data.isdigit():
+#     new_data = int(data)
+# elif data.count('.') == 1 and data.count('-') < 2 and '-' not in data[1: ] and \
+#     data.replace('.','').replace('-','').isdigit():
+#     new_data = float(data)
+# elif not data.islower():
+#     new_data = data.lower()
+# else:
+#     new_data = data.upper()
+# print([new_data])
