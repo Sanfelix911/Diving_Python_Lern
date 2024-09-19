290485130368b89cb97c741b85293d4092237409
Sanfelix <sanfelix@yandex.ru>
2024-09-19 22:46:14 +0900

    Сем3 Задание 7 решено

--- SEMINAR/Sem3_3.py
+++ SEMINAR/Sem3_3.py
@@ -65,3 +65,20 @@
 #         max_len = len(item)
 # for nn, word in enumerate(data, 1):
 #     print(f'{nn}. {word:>{max_len}}')
+
+# Задание 7
+
+# data = input('Введите строку текста: ')
+
+# my_dict1 = {}
+# for char in set(data):
+#     my_dict1[char] = data.count(char)
+# print(my_dict1)    
+
+# my_dict2 = {}
+# for char in data:
+#     if char not in  my_dict2:
+#         my_dict2[char] =1
+#     else:
+#         my_dict2[char] +=1
+# print(my_dict2)
