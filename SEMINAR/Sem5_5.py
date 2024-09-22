4db118018670da7c4948ac3f37fcda72c5161fb7
Sanfelix <sanfelix@yandex.ru>
2024-09-22 10:07:06 +0900

    Сем5 Задание 2 решено

--- SEMINAR/Sem5_5.py
+++ SEMINAR/Sem5_5.py
@@ -1,5 +1,11 @@
 # Задание1
 
-a, b, c, *d = input("Введите строку чисел через /").split("/")
-my_dict = {int(b): int(a),int(c): tuple(map(int,d))}
+# a, b, c, *d = input("Введите строку чисел через /").split("/")
+# my_dict = {int(b): int(a),int(c): tuple(map(int,d))}
+# print(my_dict)
+
+# Задание2
+
+text = 'Создайте из строки словарь, где ключ - буква, а значение - код буквы.'
+my_dict = {symbol:ord(symbol) for symbol in  set(text)}
 print(my_dict)
